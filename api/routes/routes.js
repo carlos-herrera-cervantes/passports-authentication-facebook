b9b2eba779e602e030d6ddd71a6ef1f9b8f5a568
Carlos Herrera <carlos.cervantes.cp@gmail.com>
2019-05-02 09:56:46 -0500

    Add routes.js

---
+++ api/routes/routes.js
@@ -0,0 +1,8 @@
+const routes = (app, passport) => {
+    app.get('/auth/facebook', (passport.authenticate('facebook', { scope: 'email' })));
+
+    app.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRedirecto: '/error' }),
+     (request, response) => {
+         response.redirect('/success');
+    });
+};
--- config/passports.js
+++ config/passports.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const facebookStrategy = require('passport-facebook').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
 
 const User = require('../api/models/user');
 
@@ -13,5 +13,32 @@ module.exports = passport => {
     });
     /** @endregion */
 
-    
+    /** @region_snippet_FacebookStrategy */
+    passport.use('facebook', new FacebookStrategy({
+        clientID: '303537850528292',
+        clientSecret: '5113810d343b1cc4a6e7056c8d588bca',
+        callbackURL: 'http://localhost:3000/auth/facebook/callback',
+        profileFields: ['id', 'displayName', 'emails']
+    },
+    (accessToken, refreshToken, profile, done) => {
+        let newUser = new User();
+        newUser.facebook.email = profile.emails[0].value;
+        newUser.facebook.fullName = profile.displayName;
+
+        newUser.findOne({ email: newUser.facebook.email }, (error, user) => {
+            if (!user) {
+                newUser.save((error, newUser) => {
+                    if (error) {
+                        return done(error);
+                    }
+
+                    done(null, newUser);
+                });
+            }
+            else {
+                done(null, user);
+            }
+        });
+    }));
+    /** @endregion */
 };
